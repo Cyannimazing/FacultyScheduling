fa7cf5449fe7a90f0905039ac6bfc4d9088f7042
McGillll <mcgilibag123@gmail.com>
2025-06-15 13:28:25 +0800

    web import

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 });
 
-
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+require __DIR__.'/academic-calendar.php';
+require __DIR__.'/groups.php';
+require __DIR__.'/lecturer-schedules.php';
+require __DIR__.'/lecturer-subjects.php';
+require __DIR__.'/lecturers.php';
+require __DIR__.'/program-subjects.php';
+require __DIR__.'/programs.php';
+require __DIR__.'/rooms.php';
+require __DIR__.'/subjects.php';
+require __DIR__.'/terms.php';
+require __DIR__.'/time-slots.php';
